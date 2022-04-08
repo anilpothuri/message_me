14c7e98ab32614aef236d5fd990843582e604b6c
anil kumar <siva.teki@prasthana.com>
2022-04-08 18:52:08 +0530

    message

---
+++ db/migrate/20220408104735_create_article.rb
@@ -0,0 +1,9 @@
+class CreateArticle < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :username
+      t.string :password_digest
+      t.timestamps
+    end
+  end
+end
